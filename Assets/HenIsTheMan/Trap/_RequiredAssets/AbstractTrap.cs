5536b258e735277c0635f2cdd115eb843e9bbd12
HenIsTheMan <193541T@mymail.nyp.edu.sg>
2022-01-22 14:55:24 +0800

    trap update

--- Assets/HenIsTheMan/Trap/_RequiredAssets/AbstractTrap.cs
+++ Assets/HenIsTheMan/Trap/_RequiredAssets/AbstractTrap.cs
@@ -1,42 +1,43 @@
-using System;
-using System.Collections.Generic;
 using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 
 namespace FoxHen {
     internal abstract class AbstractTrap: MonoBehaviour {
+        internal delegate void TriggerDelegate();
+
+        internal event TriggerDelegate triggerDelegate;
+
         public virtual void OnTrigger() {
-            gameObject.SetActive(false);
+            triggerDelegate?.Invoke();
         }
 
-        protected virtual void Awake() {
-            //onTriggerObservable = Observable.Create<Unit>(observer => {
-            //    observer.OnNext();
-            //    return default;
-            //});
-        }
+        protected void Awake() {
+            _ = trapAttribs.ObserveEveryValueChanged(myTrapAttribs => myTrapAttribs.lifetime)
+                .Where(lifetime => lifetime <= 0.0f)
+                .Subscribe(_ => {
+                    gameObject.SetActive(false);
+                });
 
-        internal void AddAffectableGameObj(object owner, GameObject gameObj) {
-            if(!affectableDict.ContainsKey(owner)) {
-                affectableDict.Add(owner, new List<GameObject>());
+            if(trapAttribs.shldUseLifetime) {
+                _ = this.UpdateAsObservable()
+                    .Subscribe(_ => {
+                        trapAttribs.lifetime -= Time.deltaTime;
+                    });
             }
 
-            affectableDict[owner].Add(gameObj);
+            AwakeFunc();
         }
 
-        internal void RemoveAffectableGameObj(object owner, GameObject gameObj) {
-            if(!affectableDict.ContainsKey(owner)) {
-                return;
+        protected abstract void AwakeFunc();
+
+        private void OnCollisionEnter(Collision collision) {
+            if((collision.gameObject.layer & trapAttribs.layerMask) != 0) {
+                trapAttribs.lifetime = 0.0f;
             }
-
-            _ = affectableDict[owner].Remove(gameObj);
         }
-
-        protected internal IObservable<Unit> onTriggerObservable;
 
         [SerializeField]
         private TrapAttribs trapAttribs;
-
-        private Dictionary<object, List<GameObject>> affectableDict;
     }
 }
--- Assets/HenIsTheMan/Trap/_RequiredAssets/TrapAttribs.cs
+++ Assets/HenIsTheMan/Trap/_RequiredAssets/TrapAttribs.cs
@@ -8,21 +8,12 @@ namespace FoxHen {
 	)]
 	internal sealed class TrapAttribs: ScriptableObject {
 		[SerializeField]
-		internal float maxHealth;
+		internal LayerMask layerMask;
 
 		[SerializeField]
-		internal float currHealth;
+		internal bool shldUseLifetime;
 
 		[SerializeField]
-		internal float rangeDmgRange; //Dist rangeDmg will go
-
-		[SerializeField]
-		internal float rangeDmg;
-
-		[SerializeField]
-		internal float triggerRange; //Range in which the trap will be triggered
-
-		[SerializeField]
-		internal float triggerDmg;
+		internal float lifetime;
 	}
 }
