b565fe6de6f8777c5314f29e960000c3d675bea2
HenIsTheMan <193541T@mymail.nyp.edu.sg>
2022-01-22 09:39:39 +0800

    [SAVE] AbstractTrap.cs

--- Assets/HenIsTheMan/Trap/AbstractTrap.cs
+++ Assets/HenIsTheMan/Trap/AbstractTrap.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using UniRx;
 using UnityEngine;
 
 namespace FoxHen {
@@ -6,14 +9,43 @@ namespace FoxHen {
             gameObject.SetActive(false);
         }
 
+        protected virtual void Awake() {
+            //onTriggerObservable = Observable.Create<Unit>(observer => {
+            //    observer.OnNext();
+            //    return default;
+            //});
+        }
+
+        internal void AddAffectableGameObj(object owner, GameObject gameObj) {
+            if(!affectableDict.ContainsKey(owner)) {
+                affectableDict.Add(owner, new List<GameObject>());
+            }
+
+            affectableDict[owner].Add(gameObj);
+        }
+
+        internal void RemoveAffectableGameObj(object owner, GameObject gameObj) {
+            if(!affectableDict.ContainsKey(owner)) {
+                return;
+            }
+
+            _ = affectableDict[owner].Remove(gameObj);
+        }
+
+        protected internal IObservable<Unit> onTriggerObservable;
+
         protected float maxHealth;
 
         protected float currHealth;
 
-        protected float range;
+        protected float rangeDmgRange; //Dist rangeDmg will go
 
         protected float rangeDmg;
 
+        protected float triggerRange; //Range in which the trap will be triggered
+
         protected float triggerDmg;
+
+        private Dictionary<object, List<GameObject>> affectableDict;
     }
 }
