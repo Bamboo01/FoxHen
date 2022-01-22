d8c9c39f89dcc6710d069b433b4444d93f3c7d15
olengs <limjunchen37@gmail.com>
2022-01-22 20:55:29 +0800

    Player Attributes now working correctly

--- Assets/HenIsTheMan/Trap/SlowTrap/SlowTrap.cs
+++ Assets/HenIsTheMan/Trap/SlowTrap/SlowTrap.cs
@@ -1,4 +1,9 @@
 namespace FoxHen {
     internal sealed class SlowTrap: AbstractTrap {
+
+        private void OnTriggerEnter2D(UnityEngine.Collider2D collision)
+        {
+            collision.gameObject.GetComponent<PlayerAttributes>()?.AddStatus(Status.slowed);
+        }
     }
 }
