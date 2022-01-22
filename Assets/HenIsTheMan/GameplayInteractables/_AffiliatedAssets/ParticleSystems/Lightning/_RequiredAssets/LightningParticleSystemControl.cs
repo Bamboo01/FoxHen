bb62c0ce6843de711dab5d0c3628542cf5a949cc
HenIsTheMan <193541T@mymail.nyp.edu.sg>
2022-01-23 01:03:42 +0800

    LightningParticleSystemControl.cs (cool but nah)

--- Assets/HenIsTheMan/GameplayInteractables/_AffiliatedAssets/ParticleSystems/Lightning/_RequiredAssets/LightningParticleSystemControl.cs
+++ Assets/HenIsTheMan/GameplayInteractables/_AffiliatedAssets/ParticleSystems/Lightning/_RequiredAssets/LightningParticleSystemControl.cs
@@ -1,3 +1,4 @@
+using System;
 using UniRx;
 using UnityEngine;
 
@@ -12,14 +13,19 @@ namespace FoxHen {
         [SerializeField]
         private int particleCount;
 
+        [SerializeField]
+        private float spawnIntervalInSec;
+
         private void Awake() {
             if(playerTransform != null) {
                 _ = playerTransform.ObserveEveryValueChanged(myTransform => myTransform.position.x)
                     .Skip(1)
-                    .Buffer(1)
+                    .Buffer(TimeSpan.FromSeconds(spawnIntervalInSec))
+                    .Where(myList => myList.Count > 0)
                     .Subscribe(_ => {
                         lightningParticleSystem.Emit(particleCount);
-                    });
+                    })
+                    .AddTo(this);
             }
         }
     }
