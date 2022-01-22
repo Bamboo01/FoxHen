05032cdbf0419e711de6ef4db7d622f7f5801297
Bamboo01 <48957444+Bamboo01@users.noreply.github.com>
2022-01-22 22:06:52 +0800

    2D Level Changes

---
+++ Assets/Sherwyn_Test/Scripts/BackdropScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackdropScript : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] PolygonCollider2D polygonCollider;
+
+    void Start()
+    {
+        polygonCollider.points[0] = 
+    }
+}
