06761c90b3bfdfb52d0011cd41c5be787e67cf6a
HenIsTheMan <193541T@mymail.nyp.edu.sg>
2022-01-23 14:30:43 +0800

    MysteryBox (done)

--- Assets/HenIsTheMan/GameplayInteractables/MysteryBox/_RequiredAssets/MysteryBox.cs
+++ Assets/HenIsTheMan/GameplayInteractables/MysteryBox/_RequiredAssets/MysteryBox.cs
@@ -1,15 +1,8 @@
-using UnityEngine;
-
 namespace FoxHen {
     internal sealed class MysteryBox: AbstractGameplayInteractable {
-        [SerializeField]
-        private LayerMask layerMask;
-
         private void OnEnable() {
             triggerDelegate += (other) => {
-                if((layerMask.value & (1 << other.gameObject.layer)) != 0) {
-                    other.GetComponent<PlayerInventory>().AddRandomItem();
-                }
+                _ = other.GetComponent<PlayerInventory>()?.AddRandomItem();
             };
         }
     }
